d775ea08702b31af622cb309711649d5fc91a43c
unknown <maziardehghani1380@gmail.com>
2024-01-27 23:18:41 +0330

    add interface

--- Core/Message.php
+++ Core/Message.php
@@ -19,22 +19,19 @@ class Message
         $this->update = json_decode($update, true);
 
         $this->record($this->update);
-//        $this->autoAction($this->update['message']['text']);
-//        $this->autoAction($this->update['callback_query']['data']);
         $this->autoAction($this->update);
 
     }
     public function autoAction($action): void
     {
-        if ($action['message']['text'] == '/start')
-            Start::handle($this->update);
-
-        elseif ($action['callback_query']['data'] === 'cancelAd')
+        if ($action['callback_query']['data'] === 'cancelAd')
             CancelAd::handle($this->update);
 
-        elseif($action['message']['text'] != '/start' && $action['callback_query']['data'] != 'cancelAd')
+        if (!isset($action['message']['text']))
             AdRegister::handle($this->update);
 
+
+        $this->actions[$action['message']['text']]::handle($this->update);
     }
 
     public function record($value): void
